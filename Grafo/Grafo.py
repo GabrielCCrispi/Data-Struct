aebd5782f61105df269f3aff57371a00ea670803
GabrielCrispi <GabrielCrispi02@hotmail.com>
2023-05-10 21:32:18 -0300

    Testando o grafo

--- Grafo/Grafo.py
+++ Grafo/Grafo.py
@@ -1,13 +1,36 @@
 class Vertex:
     
     def __init__(self,data):
-        
+
         self.data = data
 
 class Graph:
     
-    def __init__(self):
+    def __init__(self, directed = False):
         
         self.verticesList = []
         self.adjList = []
         
+        self.directed = directed
+    
+    def addVertex(self, data):
+        
+        vertice = Vertex(data)
+        
+        self.verticesList.append( vertice )
+        
+        self.adjList.append( [] )
+        
+# testa o grafo
+grafo1 = Graph( directed = False )
+grafo1.addVertex('A')
+grafo1.addVertex('B')
+grafo1.addVertex('C')
+grafo1.addVertex('D')
+grafo1.addVertex('E')
+grafo1.addVertex('F')
+grafo1.addVertex('G')
+grafo1.addVertex('H')
+
+print(grafo1.verticesList)
+print(grafo1.adjList)
