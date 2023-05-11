37325cd1a98708ad14e830dff27e99b33deccabf
GabrielCrispi <GabrielCrispi02@hotmail.com>
2023-05-10 21:18:52 -0300

    Praticando Grafo Estrutura de Dados

---
+++ Grafo/Grafo.py
@@ -0,0 +1,13 @@
+class Vertex:
+    
+    def __init__(self,data):
+        
+        self.data = data
+
+class Graph:
+    
+    def __init__(self):
+        
+        self.verticesList = []
+        self.adjList = []
+        
