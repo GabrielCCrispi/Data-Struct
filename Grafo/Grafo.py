006a6b283a3d208dec9aeb98c50a41822d137284
GabrielCrispi <GabrielCrispi02@hotmail.com>
2023-05-10 22:29:51 -0300

    Exercicio completo Grafo

--- Grafo/Grafo.py
+++ Grafo/Grafo.py
@@ -21,6 +21,46 @@ class Graph:
         
         self.adjList.append( [] )
         
+    def addEdge(self, orig, dest):
+        
+        idxOrig = -1
+        idxDest = -1 
+        
+        for idx, vertex in enumerate(self.verticesList):
+            
+            if vertex.data == orig:
+                idxOrig = idx
+            
+            if vertex.data == dest:
+                idxDest = idx
+                
+        if idxOrig == -1 or idxDest == -1:
+            return
+        
+        destVertex = self.verticesList[idxDest]
+        self.adjList[idxOrig].append(destVertex)
+        
+        if self.directed is False:
+            origVertex = self.verticesList[idxOrig]
+            self.adjList[idxDest].append(origVertex)
+        
+    
+    def __str__(self):
+        
+        info = ""
+        for i, vertex in enumerate(self.verticesList):
+            
+            info += '\n' + str(vertex.data) + ': '
+            
+            adjacentes = self.adjList[i]
+            info += "["
+            for j, vertexAdj in enumerate( adjacentes ):
+                info += " " + str(vertexAdj.data) 
+            info += " ]"
+        
+        return info 
+    
+   
 # testa o grafo
 grafo1 = Graph( directed = False )
 grafo1.addVertex('A')
@@ -32,5 +72,14 @@ grafo1.addVertex('F')
 grafo1.addVertex('G')
 grafo1.addVertex('H')
 
-print(grafo1.verticesList)
-print(grafo1.adjList)
+grafo1.addEdge('E','A')
+grafo1.addEdge('A','B')
+grafo1.addEdge('B','F')
+grafo1.addEdge('F','C')
+grafo1.addEdge('C','G')
+grafo1.addEdge('F','G')
+grafo1.addEdge('F','H')
+grafo1.addEdge('H','D')
+grafo1.addEdge('C','D')
+
+print(grafo1)
